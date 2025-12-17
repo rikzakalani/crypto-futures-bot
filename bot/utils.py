0418d5542d8153f2a5db47dde137a4bf342ef66e
jusnky <rikzakalani@gmail.com>
2025-12-17 17:12:13 +0700

    Add function to calculate support and resistance levels

---
+++ bot/utils.py
@@ -0,0 +1,10 @@
+def calc_support_resistance(df, window=20):
+    supports, resistances = [], []
+
+    for i in range(window, len(df) - window):
+        if df.low.iloc[i] == df.low.iloc[i-window:i+window].min():
+            supports.append(float(df.low.iloc[i]))
+        if df.high.iloc[i] == df.high.iloc[i-window:i+window].max():
+            resistances.append(float(df.high.iloc[i]))
+
+    return supports[-2:], resistances[-2:]
